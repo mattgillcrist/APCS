9e687f3ad6650a6de9ee0e06b10ff1d56aafeac7
sigmadaax <matthew@gillcrist.com>
2016-05-11 14:52:09 -0700

    mixColumns

--- GroupProject/src/AESCipher.java
+++ GroupProject/src/AESCipher.java
@@ -1,8 +1,11 @@
 
 public interface AESCipher {
+
 	public void encrypt(String text, String key);
 
-	public void decrypt(byte[][] cipher, String key);
+	public void decrypt(int[][] cipher, String key);
 
-	public byte[][] shiftRows(byte[][] state);
+	public int[][] shiftRows(int[][] state);
+
+	public int[][] mixColumns(int[][] state);
 }

e97f9a12740e11f820688b82fc80bc9f4d322c78
sigmadaax <matthew@gillcrist.com>
2016-05-16 14:59:07 -0700

    Test

---
+++ GroupProject/src/AESTest.java
@@ -0,0 +1,12 @@
+
+public class AESTest {
+	public static void main(String[] args) {
+		Transmit test = new Transmit();
+		
+		//this is a test state
+		byte[][] state = {{(byte)01110100,(byte)01101000,(byte)01101001,(byte)01110011},
+				{(byte)01101001,(byte)01110011,(byte)01100001,(byte)01110100},
+				{(byte),(byte),(byte),(byte)},
+				{(byte),(byte),(byte),(byte)}};
+	}
+}
